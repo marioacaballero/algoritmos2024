e39cebd5f992400760b41957c6d7de3347196de6
mario <mario.caballero.948@gmail.com>
2024-09-03 19:14:55 -0300

    feat: initial files

---
+++ test.pas
@@ -0,0 +1,12 @@
+
+Program test;
+
+Var 
+  num1, num2, suma: integer;
+Begin
+  WriteLn('Hello, World!');
+  ReadLn(num1);
+  ReadLn(num2);
+  suma := num1 + num2;
+  WriteLn('La suma de ', num1, ' y ', num2, ' es ', suma);
+End.
